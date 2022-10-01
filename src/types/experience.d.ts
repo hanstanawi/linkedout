df19c47fd58c67751d557e965e09697d02f6d53f
Hans Tanawi <hans.tanawi@outlook.com>
2022-10-01 17:33:16 +0800

    feat: add experiences api functions

--- src/types/experience.d.ts
+++ src/types/experience.d.ts
@@ -1,12 +1,16 @@
-interface IExperience {
-  id: string;
+interface IExperienceDto {
   jobTitle: string;
-  createdAt: string;
-  updatedAt: string;
   startDate: string;
   endDate: string | null;
   companyName: string;
   companyLogo: string | null;
   jobDescription: string;
+  isCurrent: boolean;
   userId: string;
+}
+
+interface IExperience extends IExperienceDto {
+  id: string;
+  createdAt: string;
+  updatedAt: string;
 }
