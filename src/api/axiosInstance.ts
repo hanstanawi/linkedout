a16bf87bfd5b7aa97b9aa3575ba9849b0206f9bb
Hans Tanawi <hans.tanawi@outlook.com>
2022-10-01 14:10:40 +0800

    feat: add base axios instance

---
+++ src/api/axiosInstance.ts
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+const API_BASE_URL = import.meta.env.API_BASE_URL as string;
+
+const axiosInstance = axios.create({
+  baseURL: `${API_BASE_URL}/api/v1`,
+});
+
+export default axiosInstance;
