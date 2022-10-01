fb7247ed40ee738baeb67d1162f748636170648d
Hans Tanawi <hans.tanawi@outlook.com>
2022-10-01 14:35:11 +0800

    feat: add types

---
+++ src/types/experience.d.ts
@@ -0,0 +1,12 @@
+interface IExperience {
+  id: string;
+  jobTitle: string;
+  createdAt: string;
+  updatedAt: string;
+  startDate: string;
+  endDate: string | null;
+  companyName: string;
+  companyLogo: string | null;
+  jobDescription: string;
+  userId: string;
+}
---
+++ src/types/user.d.ts
@@ -0,0 +1,15 @@
+// Data Transfer Object
+// for POST and PUT requests
+interface IUserDto {
+  firstName: string;
+  lastName: string;
+  birthDate: string;
+  about: string | null;
+}
+
+interface IUser extends IUserDto {
+  id: string;
+  createdAt: string;
+  updatedAt: string;
+  workExperiences: IExperience[];
+}
