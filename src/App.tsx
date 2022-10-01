ca4698369fc72b65458ce1d424f9e72fb890ee4d
Hans Tanawi <hans.tanawi@outlook.com>
2022-10-01 14:36:07 +0800

    wip: adding homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,30 @@
+import { useEffect, useState } from 'react';
+import * as usersApi from 'api/users.api';
+
 function App() {
+  const [users, setUsers] = useState<IUser[]>([]);
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  const getUsers = async () => {
+    try {
+      const fetchedUsers = await usersApi.getAllUsers();
+      setUsers(fetchedUsers);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
-    <div className="text-blue text-4xl bg-blue-200">
+    <main className='text-blue text-4xl bg-blue-200'>
       <h1>Hello World</h1>
-    </div>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.firstName}</li>
+        ))}
+      </ul>
+    </main>
   );
 }
 
