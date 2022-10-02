8aa3cfbdc8c0d2d8a39ae5f9064ec32911c18615
Hans Tanawi <hans.tanawi@outlook.com>
2022-10-02 23:12:44 +0800

    feat: move main pages logic to redux

--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -1,35 +1,22 @@
-import { useEffect, useState } from 'react';
-import * as usersApi from 'api/users.api';
+import { useState } from 'react';
 import CreateUserModal from 'components/users/CreateUserModal';
 import UserCard from 'components/users/UserCard';
+import { useAppSelector } from 'hooks/useAppSelector';
+import {
+  getAllUsers,
+  getUsersError,
+  getUsersStatus,
+} from 'app/slices/users.slice';
 
 function UsersPage() {
-  const [users, setUsers] = useState<IUser[]>([]);
-  const [error, setError] = useState<string | null>(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [requestStatus, setRequestStatus] = useState<
-    'idle' | 'loading' | 'failed'
-  >('idle');
-
-  const getUsers = async () => {
-    try {
-      setRequestStatus('loading');
-      const fetchedUsers = await usersApi.getAllUsers();
-      setUsers(fetchedUsers);
-      setRequestStatus('idle');
-    } catch (err: any) {
-      setError(err.response.data.message);
-      setRequestStatus('failed');
-    }
-  };
+  const users = useAppSelector(getAllUsers);
+  const requestStatus = useAppSelector(getUsersStatus);
+  const error = useAppSelector(getUsersError);
 
   const openModalHandler = (state: boolean) => {
     setIsModalOpen(state);
   };
-
-  useEffect(() => {
-    getUsers();
-  }, []);
 
   let content;
 
@@ -39,9 +26,9 @@ function UsersPage() {
         <p>Loading...</p>
       </div>
     );
-  } else if (requestStatus === 'idle' && users.length) {
+  } else if (requestStatus === 'succeeded' && users.length) {
     content = (
-      <div className="flex lg:flex-row flex-col lg:items-start items-center lg:gap-x-8 gap-y-8">
+      <div className="flex lg:flex-row flex-col lg:items-start items-center lg:gap-x-8 gap-y-8 flex-wrap">
         {users.map((user) => (
           <UserCard key={user.id} user={user} />
         ))}
@@ -50,22 +37,27 @@ function UsersPage() {
   }
 
   return (
-    <section className="container mx-auto h-full">
-      <div className="flex justify-between py-6 items-center">
-        <h3 className="font-semibold text-2xl">Our Team</h3>
-        <button
-          type="button"
-          className="bg-blue-400 hover:bg-blue-600 text-white rounded-md text-sm font-semibold py-2 px-4"
-          onClick={() => setIsModalOpen(true)}
-        >
-          Add Member
-        </button>
-      </div>
-      {content}
-      {isModalOpen && (
-        <CreateUserModal setOpen={openModalHandler} isOpen={isModalOpen} />
-      )}
-    </section>
+    <>
+      <section className="container mx-auto h-full">
+        <div className="flex justify-between py-6 items-center">
+          <h3 className="font-semibold text-2xl">Our Team</h3>
+          <button
+            type="button"
+            className="bg-blue-400 hover:bg-blue-600 text-white rounded-md text-sm font-semibold py-2 px-4"
+            onClick={() => setIsModalOpen(true)}
+          >
+            Add Member
+          </button>
+        </div>
+        {content}
+      </section>
+      <CreateUserModal
+        setOpen={openModalHandler}
+        isOpen={isModalOpen}
+        mode="create"
+        user={null}
+      />
+    </>
   );
 }
 
