36ee7047ce8b1d8f6625968b07c58bc6071f04bb
Hans Tanawi <hans.tanawi@outlook.com>
2022-10-02 17:01:43 +0800

    feat: setup redux toolkit

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -26,5 +26,6 @@ module.exports = {
     'import/prefer-default-export': 0,
     'jsx-a11y/label-has-associated-control': 0,
     'react/jsx-props-no-spreading': 0,
+    'no-param-reassign': 0,
   },
 };
