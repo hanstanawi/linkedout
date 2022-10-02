601f05be8b05cd1c39d3be78e16fb4b0270efe4a
Hans Tanawi <hans.tanawi@outlook.com>
2022-10-02 08:58:58 +0800

    feat: add not found page

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
-import Layout from 'components/layout/Layout';
-import UserProfilePage from 'pages/UserProfilePage';
-import UsersPage from 'pages/UsersPage';
 import { Routes, Route } from 'react-router-dom';
+
+import Layout from 'components/layout/Layout';
+import UserProfile from 'pages/UserProfile';
+import Users from 'pages/Users';
+import NotFound from 'pages/NotFound';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index element={<UsersPage />} />
+        <Route index element={<Users />} />
         <Route path="/user">
-          <Route path=":userId" element={<UserProfilePage />} />
+          <Route path=":userId" element={<UserProfile />} />
         </Route>
+        <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
   );
