a14d56a2c63b1b36fd180dcf58d7a7ad8a11f371
Hans Tanawi <hans.tanawi@outlook.com>
2022-10-04 22:10:45 +0800

    feat: add sorting based on time

--- src/components/experiences/ExperiencesList.tsx
+++ src/components/experiences/ExperiencesList.tsx
@@ -1,5 +1,6 @@
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 import { FaPlus } from 'react-icons/fa';
+import moment from 'moment';
 import ExperienceItem from './ExperienceItem';
 import CreateExperience from './CreateExperience';
 
@@ -15,6 +16,38 @@ function ExperiencesList({ experiences, userId }: ExperiencesListProps) {
     setIsModalOpen(state);
   };
 
+  const sortedExperiences = useMemo(() => {
+    return [...experiences].sort((a, b) => {
+      return moment(b.startDate).diff(moment(a.startDate));
+    });
+  }, [experiences]);
+
+  let content = (
+    <div className="h-full flex flex-col justify-center items-center w-full gap-y-1">
+      <h3 className="text-lg font-semibold">No Experiences</h3>
+      <p className="text-sm font-light">
+        Click Add Experience to add your job experience
+      </p>
+    </div>
+  );
+
+  if (sortedExperiences.length) {
+    content = (
+      <ul className="flex flex-col w-full gap-y-4 p-4 h-full overflow-y-auto">
+        {sortedExperiences.map((experience, i) => (
+          <>
+            <ExperienceItem
+              key={experience.id}
+              experience={experience}
+              userId={userId}
+            />
+            {i !== sortedExperiences.length - 1 && <hr />}
+          </>
+        ))}
+      </ul>
+    );
+  }
+
   return (
     <>
       <section className="flex flex-col gap-y-6 justify-center items-center w-3/4 h-[80vh] mx-auto bg-white p-6 rounded-md shadow-md">
@@ -29,19 +62,8 @@ function ExperiencesList({ experiences, userId }: ExperiencesListProps) {
             Add Experience
           </button>
         </div>
-        <ul className="flex flex-col w-full gap-y-4 p-4 h-full overflow-y-auto">
-          {experiences.map((experience, i) => (
-            <>
-              {/* TODO: FIX THIS */}
-              <ExperienceItem
-                key={experience.id}
-                experience={experience}
-                userId={userId}
-              />
-              {i !== experiences.length - 1 && <hr />}
-            </>
-          ))}
-        </ul>
+
+        {content}
       </section>
       {isModalOpen && (
         <CreateExperience
